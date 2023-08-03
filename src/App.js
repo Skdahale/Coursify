500da0ff4d4ad9d3b131192835b795466b753d73
Skdahale <97754817+Skdahale@users.noreply.github.com>
2023-08-03 13:35:43 +0530

    ain

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // import logo from './logo.svg';
-// import { Route, BrowserRouter as Router, Routes  } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes  } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     
-        <Home/>
-        
+      <Router>
+         <Routes>
+          
+          <Route exact path="/Home" element={<Home/>}></Route>
+         </Routes>
+
+      </Router>
       
     </div>
   );
